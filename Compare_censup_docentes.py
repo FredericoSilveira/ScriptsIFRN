ee3ac8130f9bc0b187b073a382783efd3077eb84
FredericoSilveira <30634727+FredericoSilveira@users.noreply.github.com>
2023-04-18 15:01:13 -0300

    script de comparação de docentes do censup
    v2

--- Compare_censup_docentes.py
+++ Compare_censup_docentes.py
@@ -13,27 +13,36 @@ with open('/Users/fred/Downloads/arquivo1.txt', 'r') as f1:
 # abre os arquivos de entrada e saída
 with open('/Users/fred/Downloads/arquivo1.txt', 'r') as f1, open('/Users/fred/Downloads/arquivo2.txt', 'r') as f2, \
         open('/Users/fred/Downloads/output1.txt', 'w') as out1, open('/Users/fred/Downloads/output2.txt', 'w') as out2:
+    # flag para indicar se a última linha lida do arquivo2.txt foi uma linha 31 com valor não encontrado
+    last_line_31_not_found = False
+
     # lê as linhas do arquivo2.txt
     for line in f2:
         if line.startswith('30|'):
             # se a linha começa com '30', insere no output1.txt
             out1.write(line)
+            # reseta a flag para indicar que a última linha lida não foi uma linha 31 com valor não encontrado
+            last_line_31_not_found = False
         elif line.startswith('31|'):
             # se a linha começa com '31', verifica se o valor da terceira coluna está em values1
             value = get_values(line)
             if value not in values1:
                 out1.write(line)
-                # se o valor não for encontrado em values1, ignora todas as linhas subsequentes começando com '32'
-                for line in f2:
-                    if line.startswith('32|'):
-                        continue
-                    else:
-                        break
+                # define a flag para indicar que a última linha lida foi uma linha 31 com valor não encontrado
+                last_line_31_not_found = True
             else:
-                # se o valor for encontrado em values1, insere todas as linhas subsequentes começando com '32' no output2.txt
+                # se o valor for encontrado em values1, insere a linha no output2.txt
                 out2.write(line)
-                for line in f2:
-                    if line.startswith('32|'):
-                        out2.write(line)
-                    else:
-                        break
+                # reseta a flag para indicar que a última linha lida não foi uma linha 31 com valor não encontrado
+                last_line_31_not_found = False
+        elif line.startswith('32|'):
+            # se a linha começa com '32', verifica se a flag de última linha 31 com valor não encontrado foi definida
+            if last_line_31_not_found:
+                # se a flag foi definida, escreve a linha no output1.txt e continua verificando as próximas linhas 32
+                out1.write(line)
+            else:
+                # se a flag não foi definida, escreve a linha no output2.txt e continua verificando as próximas linhas 32
+                out2.write(line)
+        else:
+            # ignora qualquer outra linha que não começa com '30|', '31|', ou '32|'
+            continue
